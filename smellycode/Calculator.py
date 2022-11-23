407c14f6f8690d5f456b155dc617da623dc93c53
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2022-11-23 16:51:47 +0900

    add code smells

--- smellycode/Calculator.py
+++ smellycode/Calculator.py
@@ -12,8 +12,15 @@ class Calculator:
         return b
 
     look_up = False
-    def lookup(self): # method name does not fit
+    def look_up(self): # method name does not fit
         return self.look_up
 
-    def one_(a1, a2, a3):
+    def one_variable_is_not_used(self, a1, a2, a3):
         return a1*a2
+
+    def the_first_variable_should_be_self(yours):
+        return None
+
+    def not_implemented_method(self):
+        pass
+
